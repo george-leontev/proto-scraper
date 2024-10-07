f6fe60e5db547479ef9859c8c36a9b3d3458114a
leo <egorleontev54@gmail.com>
2024-10-07 10:35:36 +0300

    feat: selenium starting

--- Scraper.Web/Services/BackgroundTickService.cs
+++ Scraper.Web/Services/BackgroundTickService.cs
@@ -1,3 +1,6 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using Scraper.DataAccess;
 using Scraper.Domain.Models;
 using System.Threading;
@@ -21,26 +24,50 @@ public class BackgroundTickService : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var timer = new Timer(Callback,
-            state: stoppingToken,
-            dueTime: 10000,
-            period: 10000
-        );
-
+        // var timer = new Timer(
+        //     Callback,
+        //     state: stoppingToken,
+        //     dueTime: 10000,
+        //     period: 60000
+        // );
+        Callback(stoppingToken);
         await Task.CompletedTask;
+    }
+
+    private void RandomTimeout(int minDuration, int maxDuration)
+    {
+        var rand = new Random();
+        Thread.Sleep(rand.Next(3000, 5000));
     }
 
     private async Task DoProcessingAsync(CancellationToken stoppingToken)
     {
-        using var scope = Services.CreateScope();
-        var databaseContext = scope.ServiceProvider.GetRequiredService<AppDataContext>();
+        string hubUrl = "http://localhost:4444/wd/hub";
+        var options = new ChromeOptions();
+        options.AddArguments([
+            "--disable-blink-features=AutomationControlled",
+            "--disable-dev-shm-usage",
+            "--disable-gpu",
+            "--window-size=1920,1080"
+        ]);
+        options.AddExcludedArgument("enable-automation");
+        options.AddAdditionalOption("useAutomationExtension", false);
+        options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36");
 
-        await databaseContext.AddAsync(new ProductDataModel
-        {
-            Name = "Iphone 15",
-            Price = 150000
-        }, stoppingToken);
 
-        await databaseContext.SaveChangesAsync(stoppingToken);
+        var webDriver = new RemoteWebDriver(new Uri(hubUrl), options.ToCapabilities(), TimeSpan.FromSeconds(180));
+        webDriver.Manage().Window.Maximize();
+
+        await webDriver.Navigate().GoToUrlAsync("https://megamarket.ru");
+
+        // webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+        RandomTimeout(3000, 5000);
+
+        Screenshot screenshot = ((ITakesScreenshot)webDriver).GetScreenshot();
+        screenshot.SaveAsFile($"/home/leo/Pictures/{Guid.NewGuid()}.png");
+        webDriver.Quit();
+
+
+        await Task.CompletedTask;
     }
 }
